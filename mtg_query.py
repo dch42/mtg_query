b124e554b9247632610146e430833c796733c7ca
ddch42 <85430824+dch42@users.noreply.github.com>
2021-12-25 02:52:32 -0600

    add card detail md

--- mtg_query.py
+++ mtg_query.py
@@ -2,12 +2,13 @@
 """CLI script to conditionally query Magic: The Gathering API using the Python SDK for card data."""
 import argparse
 from mtgsdk import Card
-from tqdm import tqdm
 import pyfiglet
 from rich.console import Console
 from rich.table import Table
+from rich.markdown import Markdown
 
 cards = []
+detailed_cards = []
 
 # define and parse args
 parser = argparse.ArgumentParser(
@@ -25,7 +26,7 @@ parser.add_argument("-cmc", type=int,
 args = parser.parse_args()
 
 # convert args to query-friendly format
-NAMES, COLORS, TYPES, TEXT = '', '', '', ''
+CMC, NAMES, COLORS, TYPES, TEXT = '', '', '', '', ''
 if args.name:
     NAMES = (','.join(args.name))
 if args.type:
@@ -34,6 +35,9 @@ if args.color:
     COLORS = (','.join(args.color))
 if args.text:
     TEXT = (','.join(args.text))
+if args.cmc:
+    CMC = int(args.cmc)
+
 
 def print_table(cards):
     """Print a table of cards matching query"""
@@ -44,14 +48,49 @@ def print_table(cards):
     table.add_column("CMC", justify="right", style="green")
     table.add_column("Mana Cost", justify="right", style="white")
     for idx, card in enumerate(cards):
-        table.add_row(f"{idx}", f"{card.name}",f"{card.set}",f"{int(card.cmc)}", f"{card.mana_cost}")
+        table.add_row(f"{idx}", f"{card.name}", f"{card.set}",
+                      f"{int(card.cmc)}", f"{card.mana_cost}")
     console = Console()
     console.print(table)
+    detailed_cards = input(
+        """\nInput card ID number to see more detailed information.\
+            \nMultiple card IDs can be entered at once, separated by spaces.\
+            \nHit `ENTER` when done, or `CTRL+C` to quit: """).split()
+    print_details(detailed_cards)
+
+
+def print_card_md(card):
+    """Create and print detailed markdown for cards"""
+    # eval whether to show loyalty, p/t, or none
+    loyal_power = f'Loyalty: {card.loyalty}' if 'Planeswalker' in card.type else f'Power/Toughness: {card.power}/{card.toughness}' if 'None' not in f'str({card.power})' else ''
+    card_md = f"""# {card.name} {card.mana_cost}
+### {card.type} - {card.rarity}
+Text: {card.text}\n
+Flavor: *{card.flavor}*
+{loyal_power}
+- CMC: {int(card.cmc)}
+- Colors: {card.colors}
+- Set: {card.set_name} ({card.set})
+"""
+    console = Console()
+    md = Markdown(card_md)
+    console.print(md)
+
+
+def print_details(detailed_cards):
+    """Loop through cards"""
+    for card_id in detailed_cards:
+        card = cards[int(card_id)]
+        print_card_md(card)
+    input("Press `ENTER` to return to card list, `CTRL+C` to quit")
+    print_table(cards)
+
 
 if __name__ == '__main__':
     pyfiglet.print_figlet("MTG Query")
     print("Searching for matching cards...\n")
-    cards = Card.where(name=f'{NAMES}', type=f'{TYPES}', colors=f'{COLORS}', cmc=f'{int(args.cmc)}', text=f'{TEXT}').all()
+    cards = Card.where(name=f'{NAMES}', type=f'{TYPES}',
+                       colors=f'{COLORS}', cmc=f'{CMC}', text=f'{TEXT}').all()
     if len(cards):
         print_table(cards)
     else:
